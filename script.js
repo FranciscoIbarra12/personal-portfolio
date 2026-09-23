5b13203b7276a360f73e896bb142fc9e3062dbc4
FranciscoIbarra12 <franciscogibarra122707@gmail.com>
2026-09-23 21:48:47 +0000

    ab

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const button = document.querySelector("#fact-button");
 const fact = document.querySelector("#fun-fact");
+const imageButton = document.querySelector("#image-toggle");
+const image = document.querySelector("img");
 
 button.addEventListener('click', function() {
     fact.hidden = !fact.hidden;
+});
+
+imageButton.addEventListener('click', function() {
+    image.hidden = !image.hidden;
+    imageButton.textContent = image.hidden ? "Show image" : "Hide image";
 });
